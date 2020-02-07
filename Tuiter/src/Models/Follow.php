9d2f1d87ac477906ddeed4d91b116c6b3dd81f10
EliBuituron <eliel.buituron@gmail.com>
2020-02-07 11:55:27 -0300

    FollowService by Eli, Edus y Nicolo

--- Tuiter/src/Models/Follow.php
+++ Tuiter/src/Models/Follow.php
@@ -3,5 +3,25 @@
 namespace Tuiter\Models;
 
 class Follow {
+    private $followId;
+    private $followerId;
+    private $followedId;
+
+    public function __construct(String $followId,String $followerId,String $followedId){
+        $this->followId=$followId;
+        $this->followerId=$followerId;
+        $this->followedId=$followedId;
+    }
     
+    public function getFollowId(): string{
+        return $this->followId;
+    }
+
+    public function getFollowerId(): string{
+        return $this->followerId;
+    }
+
+    public function getFollowedId(): string{
+        return $this->followedId;
+    }
 }

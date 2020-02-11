a54f4dc53ec5a83fa57c07099173bb9160a3e073
while-dante <dantepeluso@gmail.com>
2020-02-11 13:20:28 -0300

    cambaindo el index

--- Tuiter/src/Controllers/IndexController.php
+++ Tuiter/src/Controllers/IndexController.php
@@ -9,17 +9,49 @@ class IndexController implements \Tuiter\Interfaces\Controller {
 
     public function config($app) {
 
-        $app->get('/', function (Request $request, Response $response, array $args) {
+        $app->get('/', function (Request $request, Response $response, array $args){
     
-            $template = $request->getAttribute("twig")->load('index.html');
-
-            $response->getBody()->write(
-                $template->render(['name' => 'Dario'])
-            );
+            $template = $request->getAttribute('twig')->load('index.html');
+        
+            if(empty($_SESSION['login']) or empty($_SESSION['registrado'])){
+                $_SESSION['login'] = False;
+                $_SESSION['registrado'] = False;
+            }
+        
+            if (!$_SESSION['login']){
+                $response->getBody()->write(
+                    $template->render([
+                        'cabecera' => 'Bienvenido',
+                        'titulo' => 'Tuiter',
+                        'usuario' => 'Nombre de Usuario',
+                        'pass' => 'Contraseña',
+                        'nombre' => 'Nombre Completo',
+                        'mensaje' => 'Incorrecto, intente de nuevo'
+                    ])
+                );
+            }elseif(!$_SESSION['registrado']){
+                $response->getBody()->write(
+                    $template->render([
+                        'cabecera' => 'Bienvenido',
+                        'titulo' => 'Tuiter',
+                        'usuario' => 'Nombre de Usuario',
+                        'pass' => 'Contraseña',
+                        'nombre' => 'Nombre Completo',
+                        'mensaje' => 'Registro fallido'
+                    ])
+                );
+            }else{
+                $response->getBody()->write(
+                    $template->render([
+                        'cabecera' => 'Bienvenido',
+                        'titulo' => 'Tuiter',
+                        'usuario' => 'Nombre de Usuario',
+                        'pass' => 'Contraseña',
+                        'nombre' => 'Nombre Completo'
+                    ])
+                );
+            }
             return $response;
         });
-
     }
-
-
 }

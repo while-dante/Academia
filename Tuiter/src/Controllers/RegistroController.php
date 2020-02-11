1c952f0ab52f439b64449f346c6b68127b38b1c0
while-dante <dantepeluso@gmail.com>
2020-02-11 13:12:53 -0300

    agregando registro controller

--- Tuiter/src/Controllers/RegistroController.php
+++ Tuiter/src/Controllers/RegistroController.php
@@ -5,6 +5,17 @@ namespace Tuiter\Controllers;
 class RegistroController implements \Tuiter\Interfaces\Controller{
 
     public function config($app){
+        $app->post('/registrar', function (Request $request, Response $response){
+            $uService = $request->getAttribute('userServie');
+
+            $usuario = $uService->register($_POST['usuario'],$_POST['nombre'],$_POST['pass']);
+            $_SESSION['registrado'] = $usuario;
+            $response = new \Slim\Psr7\Response();
+            
+            $response = $response->withStatus(302);
+            $response = $response->withHeader('Location','/');
         
+            return $response;
+        });
     }
 }

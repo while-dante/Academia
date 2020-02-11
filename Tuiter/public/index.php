8782a3f7bb09a75aa3ef928c506b265226b51427
while-dante <dantepeluso@gmail.com>
2020-02-11 09:41:49 -0300

    Modificando index.php y agregando templates

--- Tuiter/public/index.php
+++ Tuiter/public/index.php
@@ -32,10 +32,136 @@ $app->get('/', function (Request $request, Response $response, array $args) use 
     
     $template = $twig->load('index.html');
 
-    $response->getBody()->write(
-        $template->render(['name' => 'Dario'])
-    );
+    if(empty($_SESSION['login']) or empty($_SESSION['registrado'])){
+        $_SESSION['login'] = False;
+        $_SESSION['registrado'] = False;
+    }
+
+    if (!$_SESSION['login']){
+        $response->getBody()->write(
+            $template->render([
+                'cabecera' => 'Bienvenido',
+                'titulo' => 'Tuiter',
+                'usuario' => 'Nombre de Usuario',
+                'pass' => 'Contraseña',
+                'nombre' => 'Nombre Completo',
+                'mensaje' => 'Incorrecto, intente de nuevo'
+            ])
+        );
+    }elseif(!$_SESSION['registrado']){
+        $response->getBody()->write(
+            $template->render([
+                'cabecera' => 'Bienvenido',
+                'titulo' => 'Tuiter',
+                'usuario' => 'Nombre de Usuario',
+                'pass' => 'Contraseña',
+                'nombre' => 'Nombre Completo',
+                'mensaje' => 'Registro fallido'
+            ])
+        );
+    }else{
+        $response->getBody()->write(
+            $template->render([
+                'cabecera' => 'Bienvenido',
+                'titulo' => 'Tuiter',
+                'usuario' => 'Nombre de Usuario',
+                'pass' => 'Contraseña',
+                'nombre' => 'Nombre Completo'
+            ])
+        );
+    }
     return $response;
+});
+
+$app->post('/login', function (Request $request, Response $response) use ($loginService){
+
+    $usuario = $loginService->login($_POST['usuario'],$_POST['pass']);
+    $userId = $usuario->getUserId();
+
+    $response = new \Slim\Psr7\Response();
+
+    if ($_SESSION['login']){
+        $response = $response->withStatus(302);
+        $response = $response->withHeader('Location','/feed');
+    }else{
+        $response = $response->withStatus(302);
+        $response = $response->withHeader('Location','/');
+    }
+
+    return $response;
+});
+
+$app->post('/registrar', function (Request $request, Response $response) use ($userService){
+
+    $usuario = $userService->register($_POST['usuario'],$_POST['nombre'],$_POST['pass']);
+    $_SESSION['registrado'] = $usuario;
+    $response = new \Slim\Psr7\Response();
+    
+    $response = $response->withStatus(302);
+    $response = $response->withHeader('Location','/');
+
+    return $response;
+});
+
+$app->get('/feed', function (Request $request, Response $response) use ($postService,$followService,$twig){
+    $template = $twig->load('feed.html');
+
+    $followed = $followService->getFollowed($_SESSION['user']);
+    
+    $posts = array();
+    $prePosts = array();
+    foreach($followed as $usuario){
+        $prePosts[] = $postService->getAllPosts($usuario);
+
+        foreach($prePosts as $post){
+
+            foreach($post as $content){
+                $posts[] = array(
+                    'content' => $content->getContent(),
+                    'owner' => $content->getUserId()
+                );
+            }
+        }
+    }
+
+    $response->getBody()->write(
+        $template->render(array(
+                'cabecera' => 'Principal',
+            )
+        )
+    );
+
+    return $response;
+});
+
+$app->post('/post', function (Request $request, Response $response) use ($postService,$userService){
+    $activeUser = $userService->getUser($_SESSION['user']);
+    $postService->create($_POST['content'],$activeUser);
+
+    $response = new \Slim\Psr7\Response();
+    $response = $response->withStatus(302);
+    $response = $response->withHeader('Location','feed');
+
+    return $response;
+});
+
+$app->get('/user/me', function(Request $request, Response $response) use($postService,$userService,$twig){
+    $activeUser = $userService->getUser($_SESSION['user']);
+    $posts = $postService->getAllPosts($activeUser);
+
+    $template = $twig->load('user.html');
+
+    $response->getBody()->write($template->render(
+        array(
+            'cabecera' => $_SESSION['user']
+        )
+    ));
+
+    return $response;
+});
+
+$app->get('/follow', function (Request $request, Response $response) use ($followService){
+
 });
 
 $app->get('/contacto', function (Request $request, Response $response, array $args) use ($twig) {

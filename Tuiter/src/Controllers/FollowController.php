94fe32626af56d9f21a1cfbfd35e86f9b0b353bd
Pronesti <dieh.diego@gmail.com>
2020-02-12 12:56:47 -0300

    not follow yourself

--- Tuiter/src/Controllers/FollowController.php
+++ Tuiter/src/Controllers/FollowController.php
@@ -9,11 +9,13 @@ class FollowController implements \Tuiter\Interfaces\Controller {
     public function config($app){
         $app->get('/follow/{username}', function (Request $request, Response $response, array $args) {
             $userId=$args["username"];
-            $followService=$request->getAttribute("followService");
-            $loginService=$request->getAttribute("loginService");
-            $followService->follow($loginService->getLoggedUser()->getUserId(),$userId);
-            $response = $response->withStatus(302);
-            $response = $response->withHeader("Location","/".$userId);
+            if($userId !== $request->getAttribute("user")->getUserId()){
+                $followService=$request->getAttribute("followService");
+                $loginService=$request->getAttribute("loginService");
+                $followService->follow($loginService->getLoggedUser()->getUserId(),$userId);
+            }
+                $response = $response->withStatus(302);
+                $response = $response->withHeader("Location","/".$userId);
             return $response;
         });
     }
